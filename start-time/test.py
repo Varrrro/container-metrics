682a57eff5bcbb60f1578dfd172dd0f82d664da3
Víctor Vázquez <victorvazrod@gmail.com>
2020-05-02 21:52:03 +0200

    Add start time testing script

---
+++ start-time/test.py
@@ -0,0 +1,102 @@
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+
+from datetime import datetime
+from socket import socket, AF_INET, SOCK_DGRAM, timeout, gethostname, gethostbyname
+from time import sleep, time
+
+from dotenv import load_dotenv
+from paramiko import SSHClient, AutoAddPolicy
+
+
+# Read connection values from .env
+load_dotenv()
+hostname = os.getenv('REMOTE_HOST')
+username = os.getenv('REMOTE_USER')
+password = os.getenv('REMOTE_PASS')
+
+# Create SSH client to remote host
+ssh = SSHClient()
+ssh.set_missing_host_key_policy(AutoAddPolicy)
+ssh.connect(hostname=hostname, username=username, password=password)
+
+# Open UDP socket
+udp_sock = socket(AF_INET, SOCK_DGRAM)
+udp_sock.bind(('', 8080))
+udp_sock.settimeout(5)
+
+# Get local IP address
+lhostname = gethostname()
+laddr = gethostbyname(lhostname)
+
+args = sys.argv
+if len(args) != 2:
+    sys.exit('Incorrect argument: the number of iterations needs to be specified')
+
+# Start testing loop
+niter = int(args[1])
+containers = ['pinger_go']
+results = pd.DataFrame(columns=['iter', 'impl', 'start_time', 'run_time'])
+for cont in containers:
+    for i in range(0, niter):
+        ssh.exec_command(
+            f'docker-compose run --name pinger {cont} {laddr}:8080')
+
+        try:
+            udp_sock.recvfrom(1024)
+            sleep(1)  # ensure container has finished
+
+            # Retrieve timestamps from container
+            _, created, _ = ssh.exec_command(
+                "docker inspect --format='{{.Created}}' pinger")
+            _, started, _ = ssh.exec_command(
+                "docker inspect --format='{{.State.StartedAt}}' pinger")
+            _, finished, _ = ssh.exec_command(
+                "docker inspect --format='{{.State.FinishedAt}}' pinger")
+
+            # Decode stdout
+            created = created.read().decode('ascii').strip('\n')
+            started = started.read().decode('ascii').strip('\n')
+            finished = finished.read().decode('ascii').strip('\n')
+
+            # Parse to datetime objects
+            dt_created = pd.to_datetime(
+                created, format='%Y-%m-%dT%H:%M:%S.%fZ')
+            dt_started = pd.to_datetime(
+                started, format='%Y-%m-%dT%H:%M:%S.%fZ')
+            dt_finished = pd.to_datetime(
+                finished, format='%Y-%m-%dT%H:%M:%S.%fZ')
+
+            # Calculate start and run times
+            start_time = (dt_started - dt_created).total_seconds() * 1000
+            run_time = (dt_finished - dt_started).total_seconds() * 1000
+
+            # Add times to dataframe
+            results = results.append(pd.DataFrame({
+                'iter': pd.Series([i], dtype='int'),
+                'impl': pd.Series([cont], dtype='str'),
+                'start_time': pd.Series([start_time], dtype='float'),
+                'run_time': pd.Series([run_time], dtype='float')
+            }), ignore_index=True)
+        except timeout:
+            print(f'[{cont} | {i}] Request timed out')
+        finally:
+            ssh.exec_command('docker rm -f pinger')
+
+    # Show mean values for each container
+    cont_mean = results.groupby(
+        ['impl']).get_group(cont).agg({
+            'start_time': np.mean,
+            'run_time': np.mean,
+        })
+    print(f'[{cont}] Mean values:\n{cont_mean}')
+
+results.to_csv(
+    f'results/start-time-{datetime.now().strftime("%Y%M%d-%H%M%S")}.csv', index_label='id')
+print('Tests finished correctly')
+
+ssh.close()
+udp_sock.close()
